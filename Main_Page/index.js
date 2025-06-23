b9cdedcaecf3693cb23715b6d8ff209b72e54517
TookyLooky <log.timofei@gmail.com>
2025-06-23 15:00:45 +0800

    Add files via upload

---
+++ Main_Page/index.js
@@ -0,0 +1,15 @@
+
+function displayDate() {
+document.getElementById("date_value").innerHTML = Date();
+}
+
+    function displayProjects() {
+        var x = document.getElementById("projectLinks");
+                x.style.display = "block";
+
+    }
+    function hideProjects() {
+        var x = document.getElementById("projectLinks");
+                x.style.display = "none";
+
+    }
