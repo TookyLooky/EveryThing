1a1b5c14b41bab0a40d5747782af3c8c2bc6ddfa
TookyLooky <log.timofei@gmail.com>
2025-06-25 21:20:01 +0800

    Add files via upload

--- Mole_Game/mole.js
+++ Mole_Game/mole.js
@@ -3,10 +3,21 @@ let currMoleTile;
 let currPlantTile;
 let score = 0;
 let gameOver = false;
+const timing = document.getElementById("time-block");
 
 window.onload = function() {
     setGame();
+    gameOver = true;
 }
+
+function startGame() {
+    gameOver = false;
+    let x = document.getElementById("btn-start");
+    x.style.display = "none";
+    timer();
+}
+
+
 
 function setGame() {
     for (let i = 0; i<9; i++) {
@@ -16,9 +27,11 @@ function setGame() {
         document.getElementById("board").appendChild(tile);
     }
     
-    setInterval(setMole, 770);
+    setInterval(setMole, 1000);
     setInterval(setPlant, 500);
 }
+
+
 
 function getRandomTile() {
     let num = Math.floor(Math.random() * 9);
@@ -40,8 +53,8 @@ function setMole() {
         currMoleTile.innerHTML = "";
     }
 
-    let mole = document.createElement("img");
-    mole.src = "/EveryThing/Images/pngwing.com.png";
+    var mole = document.createElement("img");
+    mole.src = "/Images/pngwing.com.png";
 
     let num = getRandomTile();
     if (currPlantTile && currPlantTile.id == num) {
@@ -63,7 +76,7 @@ function setPlant() {
     }
 
     let plant = document.createElement("img");
-    plant.src = "/EveryThing/Images/pngwing.com (1).png";
+    plant.src = "/Images/pngwing.com (1).png";
 
     let num = getRandomTile();
     if (currMoleTile && currMoleTile.id == num) {
@@ -81,8 +94,10 @@ function selectTile() {
     }
 
     if (this == currMoleTile) {
+        
         score += 10;
         document.getElementById("score").innerText = score.toString();
+
         
     }
 
@@ -91,3 +106,53 @@ function selectTile() {
         gameOver = true;
     }
 }
+
+function timer() {
+    
+
+timing.style.display = "block";
+
+const startingMinutes = 0.5;
+let time = startingMinutes * 60;
+
+var countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+
+    
+
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+
+
+    countdownEl.innerHTML = minutes + ":" + seconds;
+
+    if(minutes + ":" + seconds == "0:00") {
+        gameOver = true;
+        
+    }
+    
+    else{ 
+    time--;
+    }
+
+    if (gameOver == true) {
+        const game_ended = document.getElementById("game-ended-img");
+        game_ended.style.display = "flex";
+        document.getElementById("score").innerHTML = "Final score = " + score;
+
+        
+        timing.style.display = "none"
+    }
+
+
+    
+}
+}    
+
+
