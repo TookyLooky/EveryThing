37f5fcbf1c704a65be18c3f007eb926ce649d2de
TookyLooky <log.timofei@gmail.com>
2025-06-25 21:26:28 +0800

    Update mole.js

--- Mole_Game/mole.js
+++ Mole_Game/mole.js
@@ -54,7 +54,7 @@ function setMole() {
     }
 
     var mole = document.createElement("img");
-    mole.src = "/Images/pngwing.com.png";
+    mole.src = "../Images/pngwing.com.png";
 
     let num = getRandomTile();
     if (currPlantTile && currPlantTile.id == num) {
@@ -76,7 +76,7 @@ function setPlant() {
     }
 
     let plant = document.createElement("img");
-    plant.src = "../EveryThing/Images/pngwing.com (1).png";
+    plant.src = "../Images/pngwing.com (1).png";
 
     let num = getRandomTile();
     if (currMoleTile && currMoleTile.id == num) {
