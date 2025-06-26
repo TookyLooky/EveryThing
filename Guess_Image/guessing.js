4794ba400505ad11231cb7b3aa7f63bc710df681
TookyLooky <log.timofei@gmail.com>
2025-06-26 17:40:19 +0800

    Update guessing.js

--- Guess_Image/guessing.js
+++ Guess_Image/guessing.js
@@ -13,7 +13,7 @@ function displayImage(){
 
     const imageContainer = document.getElementById("board");
     
-    const imagesArray = ["/Images/cow.webp", "/Images/dog.jpg"];    
+    const imagesArray = ["../Images/cow.webp", "../Images/dog.jpg"];    
     var num = Math.floor(Math.random() * imagesArray.length);
     myImage.src = imagesArray[num];
     myImage.alt = 'alternatives';
@@ -31,7 +31,7 @@ function GetAnswer() {
 
     const word = userInputElem.value.trim().toLowerCase();
 
-    if (imageSrc == "/Images/cow.webp") {
+    if (imageSrc == "../Images/cow.webp") {
        
         if (word === "cow") {
             alert("correct!");
@@ -41,7 +41,7 @@ function GetAnswer() {
             alert("Wrong!");
         }
    
-    }else if (imageSrc === "/Images/dog.jpg") {
+    }else if (imageSrc === "../Images/dog.jpg") {
         if (word == "dog") {
             alert("correct!");
         }
